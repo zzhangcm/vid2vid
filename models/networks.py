c2fc58fea8699c34da4ac7bcdb6cb37c018a3a15
peterzeng <peterzengcc@gmail.com>
2018-10-19 22:00:35 +0800

    test

--- models/networks.py
+++ models/networks.py
@@ -196,7 +196,9 @@ class CompositeGenerator(nn.Module):
         if self.use_fg_model:
             img_fg_feat = self.indv_up(self.indv_res(self.indv_down(input)))
             img_fg = self.indv_final(img_fg_feat)
-
+            mask_device = mask.get_device()
+            if (mask_device <=1 and gpu_id >1) or (mask_device >1 and gpu_id <=1):
+                mask = mask.to(torch.device("cpu")).cuda(gpu_id).expand_as(img_raw)
             mask = mask.cuda(gpu_id).expand_as(img_raw)
             print("pass mask")
             img_final = img_fg * mask + img_final * (1-mask) 
