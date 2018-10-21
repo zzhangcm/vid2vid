56dc49d77945c087a35487e390a79b323f8a517f
peterzeng <peterzengcc@gmail.com>
2018-10-21 16:11:00 +0800

    test

--- models/networks.py
+++ models/networks.py
@@ -87,7 +87,6 @@ class CompositeGenerator(nn.Module):
         assert(n_blocks >= 0)
         super(CompositeGenerator, self).__init__()        
         self.resample = Resample2d()
-        self.resamples = {}
         self.n_downsampling = n_downsampling
         self.use_fg_model = use_fg_model
         self.no_flow = no_flow
@@ -192,10 +191,7 @@ class CompositeGenerator(nn.Module):
             # print("img_prev:{}\t@{}".format(img_prev, img_prev.get_device()))
             imgpre_slc = img_prev[:,-3:,...].cuda(gpu_id)
             print("imgpre_slc:{}\t@{}".format(imgpre_slc, img_prev.shape))
-            if gpu_id not in self.resamples:
-                with torch.cuda.device(gpu_id):
-                    self.resamples[gpu_id] = Resample2d()
-            img_warp = self.resamples[gpu_id](imgpre_slc, flow)
+            img_warp = self.resample(imgpre_slc.cuda(1), flow.cuda(1)).cuda(gpu_id)
             # img_warp = self.resample(imgpre_slc, flow)
             print("img_warp1:{}\t@{}".format(img_warp, img_warp.shape))
             img_warp = img_warp.cuda(gpu_id)
