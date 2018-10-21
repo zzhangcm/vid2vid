981e7e36085c25b8b8b9b928f53cb88af088717f
peterzeng <peterzengcc@gmail.com>
2018-10-21 19:09:10 +0800

    test

--- models/networks.py
+++ models/networks.py
@@ -194,9 +194,9 @@ class CompositeGenerator(nn.Module):
                 self.resample_gpuid = gpu_id
                 print("resample_gpu: {}".format(gpu_id))
             imgpre_slc = img_prev[:,-3:,...].cuda(self.resample_gpuid)
-            # print("imgpre_slc:{}\t@{}".format(imgpre_slc, img_prev.shape))
+            print("imgpre_slc:{}\t@{}".format(imgpre_slc, img_prev.shape))
             img_warp = self.resample(imgpre_slc, flow.cuda(self.resample_gpuid)).cuda(gpu_id)
-            # img_warp = self.resample(imgpre_slc, flow)
+            img_warp = self.resample(imgpre_slc, flow)
 
             # print("img_warp1:{}\t@{}".format(img_warp, img_warp.shape))
             img_warp = img_warp.cuda(gpu_id)
